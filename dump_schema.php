c516493badfeda9edc58b5090c6a196f66e7919f
muhammadwaleed112 <muhammadwaleedsheikh640@gmail.com>
2026-08-18 10:44:27 +0500

    report

--- dump_schema.php
+++ dump_schema.php
@@ -3,5 +3,5 @@ require 'vendor/autoload.php';
 $app = require_once 'bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-$cols = \Illuminate\Support\Facades\Schema::getColumnListing(' sales');
+$cols = \Illuminate\Support\Facades\Schema::getColumnListing('sales');
 print_r($cols);
